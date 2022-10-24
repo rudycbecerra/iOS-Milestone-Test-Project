104a75147c27cd933561f78b606f0d6ea46a19f0
rudycbecerra <rbecerr@purdue.edu>
2022-10-24 19:12:36 -0400

    add hello world app

---
+++ SimpleApp_lesson1/SimpleApp_lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApp_lesson 1
+//
+//  Created by Rudy Becerra on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
