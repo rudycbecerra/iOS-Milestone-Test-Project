b93bd8f6b6710055d19a45149b18c6ae1006e3bd
rudycbecerra <rbecerr@purdue.edu>
2022-10-24 20:08:14 -0400

    Add lesson 2

---
+++ Lesson2/Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Rudy Becerra on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
